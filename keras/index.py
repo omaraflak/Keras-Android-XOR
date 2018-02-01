b66b0b532d55a2e216a31954e94d49e5ee909778
Omar Aflak <aflakomar@gmail.com>
2018-02-01 09:12:34 -0800

    cleaned export method

--- keras/index.py
+++ keras/index.py
@@ -8,7 +8,7 @@ from tensorflow.python.tools import freeze_graph, optimize_for_inference_lib
 
 import numpy as np
 
-def export_model_for_mobile(model_name, input_node_names, output_node_name):
+def export_model_for_mobile(model_name, input_node_name, output_node_name):
     tf.train.write_graph(K.get_session().graph_def, 'out', \
         model_name + '_graph.pbtxt')
 
@@ -24,7 +24,7 @@ def export_model_for_mobile(model_name, input_node_names, output_node_name):
         input_graph_def.ParseFromString(f.read())
 
     output_graph_def = optimize_for_inference_lib.optimize_for_inference(
-            input_graph_def, input_node_names, [output_node_name],
+            input_graph_def, [input_node_name], [output_node_name],
             tf.float32.as_datatype_enum)
 
     with tf.gfile.FastGFile('out/tensorflow_lite_' + model_name + '.pb', "wb") as f:
@@ -41,7 +41,7 @@ def main():
     model.compile(loss='binary_crossentropy', optimizer=SGD(lr=0.1))
     model.fit(X, Y, batch_size=1, nb_epoch=1000)
 
-    export_model_for_mobile('xor_nn', ["dense_1_input"], "dense_2/Sigmoid")
+    export_model_for_mobile('xor_nn', "dense_1_input", "dense_2/Sigmoid")
     model.summary()
 
 if __name__=='__main__':
